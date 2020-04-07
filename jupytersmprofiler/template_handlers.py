00fa20b3d4527392be1e758cde6dd3d44e867de7
Andrea Olgiati <olg@amazon.com>
2020-04-06 17:33:00 -0700

    More believable data

--- jupytersmprofiler/template_handlers.py
+++ jupytersmprofiler/template_handlers.py
@@ -15,32 +15,30 @@ class ProfilerTemplateHandler(RequestHandler):
   #@tornado.web.authenticated
   def get(self, name):
     # TODO: obtain actual data from S3
-    vals = generate_data( 0, 10, 10 )
-    df = pd.DataFrame(vals)
-    chart = alt.Chart(df).mark_line().encode( x='date:T', y='value:Q', color='type:N' )
+    res = []
+    for nm in [ 'cpu_0', 'cpu_1', 'gpu_0', 'gpu_1', 'network', 'disk']:
+      res.extend(signalseq( 500, 20, nm ))
+    df = pd.DataFrame(res)
+    chart = alt.Chart(df).mark_line().encode( x='date:T', y='value:Q', color='type:N' ).properties(
+      width=1200,
+      height=300
+    )
     sio = StringIO()
     chart.save(sio, format='html')
-    self.set_header('Content-Type', 'text/html')
+    #self.set_header('Content-Type', 'text/html')
     self.write(sio.getvalue())
     #self.finish(json.dumps(res))
 
 
-#def rands(x):
-#    y = 0
-#    result = []
-#    for _ in x:
-#        result.append(y)
-#        y += np.random.normal(scale=1)
-#    return np.array(result)
 
-def generate_data(time_start, time_end, item_no):
-  names = ['cpu_0', 'cpu_1', 'cpu_2', 'cpu_3',
-           'gpu_0', 'gpu_1', 'gpu_2', 'gpu_3','gpu_4', 'gpu_5', 'gpu_6', 'gpu_7',
-           'network', 'disk']
-  res = []
-  for _ in range(item_no):
-    tm = random.uniform(time_start, time_end)
-    nm = random.choice(names)
-    val = random.uniform(0, 1)
-    res.append({ 'date': tm, 'type' : nm, 'value': val })
-  return res
+def signalseq( sz, smoothfactor, nm):
+    ts = np.arange(sz) #.astype(float)
+    #ts += np.random.uniform(low=0, high=1, size=(sz))
+    rms = np.convolve(np.random.randn(sz), np.ones((smoothfactor,))/smoothfactor)[(smoothfactor-1):]
+    rms += -min(rms)
+    rms /= max(rms)
+    #plt.plot( ts, rms )
+    res = []
+    for t, rm in zip(ts,rms):
+      res.append({ 'date': t, 'value' : rm, 'type': nm})
+    return res
