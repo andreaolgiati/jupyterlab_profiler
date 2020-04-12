5be3d0ac205893178565c7eeed8bc58e6d24c8fc
Andrea Olgiati <olg@amazon.com>
2020-04-11 18:15:05 -0700

    Streaming Profiler Working

--- jupytersmprofiler/template_handlers.py
+++ jupytersmprofiler/template_handlers.py
@@ -5,6 +5,7 @@ import numpy as np
 import os
 import pandas as pd
 import random
+import sys
 import tornado
 import tornado.ioloop
 import tornado.web
@@ -12,32 +13,44 @@ import tornado.web
 from io import BytesIO
 from tornado.web import RequestHandler, HTTPError
 
+class DummyDataHandler(RequestHandler):
+
+  def initialize(self, s3res):
+    self.set_header('Content-Type', 'application/json')
+    self.s3res = s3res
+  
+  def get(self):
+    x = int(self.get_argument('x'))
+    res = [
+       {"date": x, "value": np.random.uniform(low=0,high=1), "type": 0 }, 
+       {"date": x, "value": np.random.uniform(low=0,high=1), "type": 1 }, 
+    ]
+    print( "CALL=", x)
+    self.finish(json.dumps(res))
 
 class ProfilerDataHandler(RequestHandler):
-  def prepare(self):
-    self.set_header('Content-Type', 'application/json')
-    self.s3res = boto3.resource('s3')
 
+  def initialize(self, s3res):
+    self.set_header('Content-Type', 'application/json')
+    self.s3res = s3res
+ 
   #@tornado.web.authenticated
   def get(self):
-    bucket_name = self.get_argument('bucket', None)
-    if bucket_name is None:
-       raise HTTPError(reason='No bucket specified', status_code=400)
-
-    object_name = self.get_argument('object', None)
-    if object_name is None:
-       raise HTTPError(reason='No prefix specified', status_code=400)
-
+    bucket_name = self.get_argument('bucket')
+    object_name = self.get_argument('object')
+    min_date = float(self.get_argument('min_date', sys.float_info.min))
+    max_date = float(self.get_argument('max_date', sys.float_info.max))
+    print( self.request.full_url)
+    
     bucket = self.s3res.Bucket(bucket_name)
-    if bucket is None:
-       raise HTTPError(reason=f'Bucket {bucket_name} does not exist', status_code=404)
-
     obj = bucket.Object(object_name)
-    if obj is None:
-       raise HTTPError(reason=f'Object {object_name} does not exist in bucket {bucket_name}', status_code=404)
 
     json_data = BytesIO()
     obj.download_fileobj(json_data)
+
+    full_data = json.loads(json_data.getvalue())
+    #print("FD=", full_data[0])
+
 
     # JSON should look like so:
     # [
@@ -54,7 +67,14 @@ class ProfilerDataHandler(RequestHandler):
     # - "type" is a free variable, Vega will distinguish. You can add new types
     # - the list does not need to be ordered: you can randomly shuffle this list and the viz will be identical
 
-    self.write(json_data.getvalue())
+    filtered = []
+    for x in full_data:
+      #print("X=", x )
+      d = float(x['date'])
+      if d < min_date or d > max_date:
+        continue
+      filtered.append(x)
+    self.write(json.dumps(filtered))
 
 
 def make_app():
@@ -62,8 +82,12 @@ def make_app():
     "static_path": os.getcwd(),
     "static_url_prefix": "/resources/",
   }
+
+  s3res = boto3.resource('s3')
+
   return tornado.web.Application( [
-    (r'/profiler/data', ProfilerDataHandler),
+    (r'/profiler/data', ProfilerDataHandler, dict(s3res = s3res)),
+    (r'/dummy', DummyDataHandler, dict(s3res = s3res)),
   ], 
   debug=True,
   **settings )
